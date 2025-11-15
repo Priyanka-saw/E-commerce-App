22f0b8dcb1956a1f99c4969bbd960fe2d41b13a5
Priyanka-saw <priyankasaw51@gmail.com>
2025-11-15 16:02:32 +0530

    built NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,13 @@ import Product from './pages/Product'
 import Cart from './pages/Cart'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-
+      <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/collection" element={<Collections/>} />
