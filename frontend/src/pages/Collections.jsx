f7d981de055798464abd3e58ccb468b82ae2dda4
Priyanka-saw <priyankasaw51@gmail.com>
2025-12-29 16:20:56 +0530

    collections page created

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useContext } from 'react';
+import { ShopContext } from '../context/shopContext';
 
 const Collections = () => {
+
+  const {products} = useContext(ShopContext);
+
   return (
-    <div></div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+
+      {/* filter option */}
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>Filters</p>
+
+          {/* category filters */}
+
+          <div className=''>
+            
+          </div>
+      </div>
+    </div>
   )
 }
 
